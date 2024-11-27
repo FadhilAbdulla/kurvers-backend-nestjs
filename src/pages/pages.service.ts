dd025f3fd7e846f67e77f3d82dbeb6dc0e14d113
Ubuntu <ubuntu@ip-172-26-2-221.ap-south-1.compute.internal>
2024-11-27 13:04:39 +0000

    minor changes

--- src/pages/pages.service.ts
+++ src/pages/pages.service.ts
@@ -6,7 +6,7 @@ import { Root, FooterItems } from './entities/constant';
 
 @Injectable()
 export class PagesService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly databaseService: DatabaseService) { }
 
   ConvertData(Items, Data) {
     return Data.reduce((acc, item) => {
@@ -105,14 +105,15 @@ export class PagesService {
 
   async career_apply(id: number) {
     const res = await this.databaseService.configuration.findMany();
-    // const career = await this.databaseService.job.findFirst({ where: { id: id } });
-    console.log(id);
 
-    return {
+    const data = {
       ...Root,
       jobId: id,
       footer: { ...this.ConvertData(FooterItems, res) },
     };
+
+    return data
+
   }
 
   async contact() {
